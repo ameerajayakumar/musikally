b17cb0285cbfe6670745a1cf255f834f2616714d
Ameera Jayakumar <ameeraj.mec@gmail.com>
2025-05-13 19:37:52 +0530

    user route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use('/api/users', userRoutes);
+
 app.listen(PORT, () => {
-  console.log('Server is running at port: ', PORT);
+  console.log('Server is running at port:', PORT);
 });
