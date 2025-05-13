98e71b0b9a2d06502988c16126d5eaa0963f61cb
Ameera Jayakumar <ameeraj.mec@gmail.com>
2025-05-13 19:52:35 +0530

    all routes init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import adminRoutes from './routes/admin.route.js';
+import songRoutes from './routes/song.route.js';
+import albumRoutes from './routes/album.route.js';
+import statRoutes from './routes/stat.route.js';
 
 dotenv.config();
 
@@ -8,6 +13,11 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/songs', songRoutes);
+app.use('/api/albums', albumRoutes);
+app.use('/api/stats', statRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running at port:', PORT);
