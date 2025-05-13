ff47cc8b6e43af76f4ae3105c8a03f109e1644c5
Ameera Jayakumar <ameeraj.mec@gmail.com>
2025-05-13 17:13:42 +0530

    shadcn ui & clerk integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+import { Button } from './components/ui/button';
+
 function App() {
   return (
     <>
-      <h1 className="text-green-700">Hello World!</h1>
+      <h1 className="text-green-700 ">Hello World!</h1>
+      <Button>Click me</Button>
+
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   );
 }
